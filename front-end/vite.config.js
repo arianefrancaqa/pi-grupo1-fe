97678dc3ed0d10464371e57e349c7c8183df249a
arianefrancaqa <arianefrancaqa@gmail.com>
2023-09-14 20:59:29 -0300

    test

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,24 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const defineConfig = ({
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: 'MyLib',
-      fileName: (format) => `my-lib.${format}.js`
-    },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
+      // https://rollupjs.org/guide/en/#big-list-of-options
     }
-  }
-})
+  },
+  base: '/',
+  root: './src',
+  plugins: [react()],
+});
+export default defineConfig;
