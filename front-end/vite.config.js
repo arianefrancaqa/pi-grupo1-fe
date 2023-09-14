66e5f9de80f787fccce8344ca29b2413475e4262
arianefrancaqa <arianefrancaqa@gmail.com>
2023-09-14 20:51:02 -0300

    test

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: { manifest: true, outDir: './dist' },
-  base: '/',
-  root: './src',
-  plugins: [react()],
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
