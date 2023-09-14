ccdd0e5611f78e727455ac583f47cadecfecc237
arianefrancaqa <arianefrancaqa@gmail.com>
2023-09-14 20:40:17 -0300

    test

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,19 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { manifest: true, outDir: './dist' },
+  base: '/',
+  root: './src',
   plugins: [react()],
-  define: {
-    "version": 2,
-    "builds": [
-      { "src": "package.json", "use": "@vercel/static-build", "config": { "distDir": "dist" } },
-      { "src": "serverless/*.js", "use": "@vercel/node" }
-    ],
-    "rewrites": [
-      {
-        "source": "/(.*)",
-        "destination": "/serverless/test.js"
-      }
-    ]
-  }
-
 })
