34d6c0b89022f2c33c62a12e5a9ad96dedc72308
arianefrancaqa <arianefrancaqa@gmail.com>
2023-09-14 21:45:48 -0300

    test

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -10,23 +10,23 @@ import { StyledEngineProvider } from "@mui/material/styles";
 import CalendarB from "../components/DetailsProduct/CalendarB";
 import ImageView from "../components/DetailsProduct/ImageView";
 import ShareProduct from "../components/DetailsProduct/ShareProduct";
- 
+
 const Details = () => {
 
     const { id } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [product, setProduct] = useState();
-   
+
     useEffect(() => {
-        
+
         console.log('id', id);
         console.log(`${apiBaseUrl}/produtos/${id}`);
         const loadProducts = async () => {
-            try{
-            const response = await axios.get(`${apiBaseUrl}/produtos/${id}`);
-           
-            //const product = Object.values(response.data.content);
-            setProduct(response.data)
+            try {
+                const response = await axios.get(`${apiBaseUrl}/produtos/${id}`);
+
+                //const product = Object.values(response.data.content);
+                setProduct(response.data)
 
             } catch (error) {
                 console.error("Error loading product:", error);
@@ -34,27 +34,27 @@ const Details = () => {
                 setIsLoading(false);
             }
         }
-       
+
         loadProducts();
-       
+
     }, [])
-   
+
     return (
         <div>
-        {!isLoading && product && (
-        <div>
-            <HeaderLocation product={product}/>
-            <ShareProduct product={product}/>
-            <ImageView product={product}/>
-            <PoliticaProduto product={product}/>
-            <CaracteristicaDescricao product={product}/>
-            <StyledEngineProvider injectFirst>
-                <CalendarB id={id} />
-            </StyledEngineProvider>
+            {!isLoading && product && (
+                <div>
+                    <HeaderLocation product={product} />
+                    {/* <ShareProduct product={product}/> */}
+                    <ImageView product={product} />
+                    <PoliticaProduto product={product} />
+                    <CaracteristicaDescricao product={product} />
+                    <StyledEngineProvider injectFirst>
+                        <CalendarB id={id} />
+                    </StyledEngineProvider>
+                </div>
+            )}
         </div>
-        )}
-        </div>
-      
+
     );
 };
 
